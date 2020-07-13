ddd10d8acbdf899aa4298248e01aa6539735004e
tester <aiforeveryoneai@gmail.com>
2020-07-13 15:52:59 +0500

    Update App.js
    Added navigation, remove button  and applied some refactoring to code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const PAGE_PRODUCTS = "products";
+const PAGE_CART = "cart";
+
 function App() {
   const [cart, setCart] = useState([]);
-  const [page, setPage] = useState("");
+  const [page, setPage] = useState(PAGE_PRODUCTS);
   const [products] = useState([
     {
       name: "AA Battery",
@@ -18,8 +21,15 @@ function App() {
   ]);
 
   const addToCart = (product) => {
-    console.log("We are in add to cart");
-    setCart([...cart, product]);
+    setCart([...cart, { ...product }]);
+  };
+
+  const removeFromCart = (productToRemove) => {
+    setCart(cart.filter((product) => product !== productToRemove));
+  };
+
+  const navigateTo = (nextPage) => {
+    setPage(nextPage);
   };
 
   const renderProducts = () => (
@@ -37,12 +47,34 @@ function App() {
       </div>
     </>
   );
+
+  const renderCart = () => (
+    <>
+      <h1>Cart</h1>
+      <div className="products">
+        {cart.map((product, index) => (
+          <div className="product" key={index}>
+            <h3>{product.name}</h3>
+            <h4>{product.cost}</h4>
+            <img src={product.image} alt={product.name} />
+            <button onClick={() => removeFromCart(product)}>
+              Remove from Cart
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
   return (
     <div className="App">
       <header>
-        <button>Add to cart ({cart.length})</button>
+        <button onClick={() => navigateTo(PAGE_CART)}>
+          Go to cart ({cart.length})
+        </button>
+        <button onClick={() => navigateTo(PAGE_PRODUCTS)}>View Products</button>
       </header>
-      {renderProducts()}
+      {page === PAGE_PRODUCTS && renderProducts()}
+      {page === PAGE_CART && renderCart()}
     </div>
   );
 }
