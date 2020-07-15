1d24b33066bc788f7e5f13aca92208b4615b0061
tester <aiforeveryoneai@gmail.com>
2020-07-15 07:55:05 +0500

    Update the project
    Added pictures of shoes in products.js, added some styles to App.css  and did some other changes.

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
 const Cart = ({ cart, removeFromCart }) => {
-  return (
-    <>
-      <h1>Cart</h1>
-      <div className="products">
-        {cart.map((product, index) => (
-          <div className="product" key={index}>
-            <h3>{product.name}</h3>
-            <h4>{product.cost}</h4>
-            <img src={product.image} alt={product.name} />
-            <button onClick={() => removeFromCart(product)}>
-              Remove from Cart
-            </button>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+  if (cart.length)
+    return (
+      <>
+        <h1>Cart</h1>
+        <div className="products">
+          {cart.map((product, index) => (
+            <div className="product" key={index}>
+              <h3>{product.name}</h3>
+              <h4>{product.cost}</h4>
+              <img src={product.image} alt={product.name} />
+              <button onClick={() => removeFromCart(product)}>
+                Remove from Cart
+              </button>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  else {
+    return <h1> There are no items in the cart</h1>;
+  }
 };
 
 export default Cart;
--- src/products.jsx
+++ src/products.jsx
@@ -3,14 +3,28 @@ import React, { useState } from "react";
 const Products = ({ addToCart }) => {
   const [products] = useState([
     {
-      name: "AA Battery",
-      cost: "$2.99",
-      image: "https://tinyurl.com/y8wnrymv",
+      name: "VALOUR BLUE",
+      cost: "$70.99",
+      image:
+        "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
     },
     {
-      name: "Blanket",
-      cost: "$19.99",
-      image: "https://tinyurl.com/y7s5krpw",
+      name: "JORDAN MARS 270 LONDON",
+      cost: "$85.99",
+      image:
+        "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+    },
+    {
+      name: "RACER BLUE",
+      cost: "$99.00",
+      image:
+        "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+    },
+    {
+      name: "JORDAN MAX 200",
+      cost: "$79.00",
+      image:
+        "https://secure-images.nike.com/is/image/DotCom/844940_100_99_PREM?$SNKRS_COVER_WT$&align=0,1",
     },
   ]);
 
@@ -19,7 +33,7 @@ const Products = ({ addToCart }) => {
       <h1>Products</h1>
       <div className="products">
         {products.map((product, index) => (
-          <div className="product" key={index}>
+          <div id="product-item" className="product" key={index}>
             <h3>{product.name}</h3>
             <h4>{product.cost}</h4>
             <img src={product.image} alt={product.name} />
